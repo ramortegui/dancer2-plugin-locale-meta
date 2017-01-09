ee975c213d7cc458ef2b14293e33ad1844d1f386
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-01-09 14:45:46 -0700

    Add Name, description, and synopsis to document the package.  Add dependencies.

--- lib/Dancer2/Plugin/Locale/Meta.pm
+++ lib/Dancer2/Plugin/Locale/Meta.pm
@@ -1,5 +1,42 @@
+package Dancer2::Plugin::Locale::Meta;
+
 use strict;
 use warnings;
-package Dancer2::Plugin::Locale::Meta;
+use Dancer2::Plugin;
+use Dancer2::FileUtils;
+use I18N::AcceptLanguage;
+use Locale::Meta;
+
+our $VERSION = '0.001';
+
+=head NAME
+
+Dancer2::Pluging::Locale::Meta
+
+=head DESCRIPTION
+
+This plugin allow Dancer2 developers to use L<Locale::Meta> package.  This Plugin is
+based on L<Dancer2::Plugin::Locale::Wolowitz> plugin.
+
+=head SYNOPSIS
+
+    use Dancer2;
+    use Dancer2::Plugin::Locale::Meta;
+
+    # in your templates
+
+    get '/' => sub {
+      my $greeting = loc("hello");
+      template index.tt, { greeting => $greeting }
+    }
+
+    # in your template
+
+    <% l('greeting') %>
+
+
+    
+
+
 
 1;
